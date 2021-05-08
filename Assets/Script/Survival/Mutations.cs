5375a7e05ef2380ec190a0f02bd8b81a2edb761a
Eruxe <39292686+Eruxe@users.noreply.github.com>
2021-05-08 16:52:56 +0200

    3d and uvs

---
+++ Assets/Script/Survival/Mutations.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+
+public class Mutations : MonoBehaviour
+{
+
+    public delegate float TwoVariablesFunction(float x1, float x2);
+
+    public static TwoVariablesFunction MutationSelectorY(Random Generator, int chaosFrequencyY, ref float sizex)
+    {
+        //VARIABLES POUR LES MUTATIONS
+        float copyx = sizex;
+        float intensity;
+        float frequency = (float)Math.PI * 2;
+        int negative;
+
+        if (Generator.Next(100) > chaosFrequencyY)
+        {
+            int select = Generator.Next(5);
+            negative = Generator.Next(2);
+            //SELECTEUR MANUEL CI DESSOUS
+            //select = 5;
+
+            switch (select)
+            {
+                case 0: //MONTE ET DESCENTE
+                    if (negative > 0) copyx = -sizex;
+                    return (pas, z) => Mathf.Lerp(0, copyx * 0.5f, pas);
+                case 1: //VAGUE BASSE
+                    sizex += Generator.Next(4, 9);
+                    intensity = (float)(Generator.Next(10, 20)) / 10;
+                    return (pas, z) => (intensity * Mathf.Cos(pas * frequency) - intensity);
+                case 2: //VAGUE BASSE
+                    sizex += Generator.Next(6, 11);
+                    intensity = (float)(Generator.Next(10, 20)) / 10;
+                    return (pas, z) => (intensity * Mathf.Cos(pas * frequency + (float)Math.PI) + intensity);
+                case 3: //VAGUE MONTE
+                    intensity = (float)(Generator.Next(6, 9)) / 10;
+                    frequency = 8;
+                    return (pas, z) => Mathf.Lerp(0, copyx * 0.3f + (intensity * Mathf.Sin(pas * frequency)), pas);
+                case 4: //VAGUE DESCENTE
+                    sizex += Generator.Next(3, 7);
+                    copyx = sizex;
+                    intensity = (float)(Generator.Next(7, 17)) / 10;
+                    return (pas, z) => Mathf.Lerp(0, -copyx * 0.5f + (intensity * Mathf.Sin(pas * frequency)), pas);
+            }
+        }
+        return (pas, z) => Mathf.Lerp(0, 0, pas);
+    }
+
+    public static TwoVariablesFunction MutationSelectorWidth(Random Generator, int chaosFrequencyZ, float sizez)
+    {
+        if (Generator.Next(100) > chaosFrequencyZ)
+        {
+            int nextSizeZ = Generator.Next(3, 7);
+            return (pas, z) => Mathf.Lerp(0, nextSizeZ - sizez, pas);
+        }
+        return (pas, z) => Mathf.Lerp(0, 0, pas);
+    }
+
+    public static TwoVariablesFunction MutationSelectorZ(Random Generator, int chaosFrequencyZ, ref float sizex)
+    {
+        //VAR POUR MUTATION
+        float copyx = sizex;
+        float angle;
+        float intensity;
+        float frequency;
+        int negative;
+
+        if (Generator.Next(100) > chaosFrequencyZ)
+        {
+            int select = Generator.Next(3);
+            negative = Generator.Next(2);
+            //SELECTEUR MANUEL CI DESSOUS
+            //select = 2;
+
+            switch (select)
+            {
+                case 0: //DIAGONALE DROITE
+                    angle = (float)(Generator.Next(3, 12)) / 10;
+                    if (negative > 0) copyx = -sizex;
+                    return (pas, z) => Mathf.Lerp(0, copyx * angle, pas);
+                case 1: //VAGUES DROITE
+                    sizex += Generator.Next(3, 10);
+                    copyx = sizex;
+                    intensity = (float)(Generator.Next(20, 35)) / 10;
+                    frequency = (float)(Generator.Next(5, 9));
+                    if (negative > 0) intensity = -intensity;
+                    return (pas, z) => (intensity * Mathf.Cos(pas * frequency) - intensity);
+                case 2: //VAGUE DIAGONALE
+                    angle = (float)(Generator.Next(3, 8)) / 10;
+                    sizex += Generator.Next(3, 8);
+                    copyx = sizex;
+                    if (negative > 0) copyx = -sizex;
+                    intensity = (float)(Generator.Next(20, 35)) / 10;
+                    frequency = (float)(Generator.Next(5, 9));
+                    return (pas, z) => Mathf.Lerp(0, copyx * angle + (intensity * Mathf.Sin(pas * frequency)), pas);
+            }
+        }
+        return (pas, z) => Mathf.Lerp(0, 0, pas);
+    }
+
+}
--- Assets/Script/Survival/meshProc.cs
+++ Assets/Script/Survival/meshProc.cs
@@ -18,13 +18,12 @@ public class meshProc : MonoBehaviour
     
     MeshFilter CurrentMeshFilter;
 
-    delegate float TwoVariablesFunction(float x1, float x2);
-
     public class MeshAndPos
     {
         public Mesh mesh;
         public Vector3 pos;
         public float width;
+        
 
         public MeshAndPos(Mesh mesh, Vector3 pos,float width)
         {
@@ -44,7 +43,6 @@ public class meshProc : MonoBehaviour
         Random rnd = new Random();
         int seed = rnd.Next(0, 999999999);
         Random Generator = new Random(seed);
-
 
         int nbPlat = Generator.Next(4,10);
         float sizex;
@@ -66,16 +64,16 @@ public class meshProc : MonoBehaviour
             sizex = Generator.Next(6, 9);
 
 
-            TwoVariablesFunction heighfunct = (pas, z) => Mathf.Lerp(0, 0, pas);
-            TwoVariablesFunction widthfunct = (pas, z) => Mathf.Lerp(0, 0, pas);
-            TwoVariablesFunction zfunct = (pas, z) => Mathf.Lerp(0, 0, pas);
+            Mutations.TwoVariablesFunction heighfunct = (pas, z) => Mathf.Lerp(0, 0, pas);
+            Mutations.TwoVariablesFunction widthfunct = (pas, z) => Mathf.Lerp(0, 0, pas);
+            Mutations.TwoVariablesFunction zfunct = (pas, z) => Mathf.Lerp(0, 0, pas);
             //Mutations
             if (i<nbPlat-1 && i>0)
             {
-                heighfunct = MutationSelectorY(Generator, chaosFrequencyY, ref sizex);
+                heighfunct = Mutations.MutationSelectorY(Generator, chaosFrequencyY, ref sizex);
                 int zModifier = Generator.Next(3);
-                if (zModifier <= 1) zfunct = MutationSelectorZ(Generator, chaosFrequencyZ, ref sizex);
-                else widthfunct = MutationSelectorWidth(Generator, chaosFrequencyWidth, sizez);
+                if (zModifier <= 1) zfunct = Mutations.MutationSelectorZ(Generator, chaosFrequencyZ, ref sizex);
+                else widthfunct = Mutations.MutationSelectorWidth(Generator, chaosFrequencyWidth, sizez);
             }
 
             //Centrage du spawn
@@ -94,7 +92,7 @@ public class meshProc : MonoBehaviour
             Currentprefab = Instantiate(prefab, new Vector3(cursorx, cursory, cursorz), Quaternion.identity) as GameObject;
             CurrentMeshFilter = Currentprefab.GetComponent(typeof(MeshFilter)) as MeshFilter;
 
-            MeshAndPos meshandpos = GenerateMutatedPlaneXZ(new Vector2(sizex, sizez), 30, 30, heighfunct, widthfunct, zfunct);
+            MeshAndPos meshandpos = GenerateMutatedPlaneXZ(new Vector2(sizex, sizez), 30, 30, heighfunct, widthfunct, zfunct, new Vector2(cursorx,cursorz));
 
             CurrentMeshFilter.sharedMesh = meshandpos.mesh;
 
@@ -111,113 +109,23 @@ public class meshProc : MonoBehaviour
 
     }
 
-    TwoVariablesFunction MutationSelectorY(Random Generator,int chaosFrequencyY,ref float sizex)
-    {
-        //VARIABLES POUR LES MUTATIONS
-        float copyx = sizex;
-        float intensity;
-        float frequency = (float)Math.PI * 2;
-        int negative;
-
-        if (Generator.Next(100) > chaosFrequencyY)
-        {
-            int select = Generator.Next(6);
-            negative = Generator.Next(2);
-            //SELECTEUR MANUEL CI DESSOUS
-            //select = 5;
-
-            switch (select) {
-                case 0:
-                    if (negative > 0) copyx = -sizex;
-                    return (pas, z) => Mathf.Lerp(0, copyx * 0.5f, pas);
-                case 2:
-                    sizex += Generator.Next(4,9);
-                    intensity = (float)(Generator.Next(10, 20))/10;
-                    return (theta, z) => (intensity * Mathf.Cos(theta * frequency)-intensity);
-                case 3:
-                    sizex += Generator.Next(6, 11);
-                    intensity = (float)(Generator.Next(10, 20)) / 10;
-                    return (theta, z) => (intensity * Mathf.Cos(theta * frequency + (float)Math.PI) +  intensity);
-                case 4:
-                    intensity = (float)(Generator.Next(6, 9)) / 10;
-                    frequency = 8;
-                    return (pas, z) => Mathf.Lerp(0, copyx * 0.3f + (intensity * Mathf.Sin(pas * frequency)), pas);
-                case 5:
-                    sizex += Generator.Next(3,7);
-                    copyx = sizex;
-                    intensity = (float)(Generator.Next(7, 17)) / 10;
-                    return (pas, z) => Mathf.Lerp(0, -copyx * 0.5f + (intensity * Mathf.Sin(pas * frequency)), pas);
-            }
-        }
-        return (pas, z) => Mathf.Lerp(0, 0, pas);
-    }
-
-    TwoVariablesFunction MutationSelectorWidth(Random Generator,int chaosFrequencyZ,float sizez)
-    {
-        if (Generator.Next(100) > chaosFrequencyZ)
-        {
-           int nextSizeZ = Generator.Next(2, 7);
-           return (pas, z) => Mathf.Lerp(0, nextSizeZ - sizez, pas);
-        }
-        return (pas, z) => Mathf.Lerp(0, 0, pas);
-    }
-
-    TwoVariablesFunction MutationSelectorZ(Random Generator, int chaosFrequencyZ, ref float sizex)
-    {
-        //VAR POUR MUTATION
-        float copyx = sizex;
-        float angle;
-        float intensity;
-        float frequency;
-        int negative;
-
-        if (Generator.Next(100) > chaosFrequencyZ)
-        {
-            int select = Generator.Next(3);
-            negative = Generator.Next(2);
-            //SELECTEUR MANUEL CI DESSOUS
-            //select = 0;
-
-            switch (select){
-                case 0:
-                    angle = (float)(Generator.Next(3, 12)) / 10;
-                    if (negative > 0) copyx = -sizex;
-                    return (pas, z) => Mathf.Lerp(0, copyx * angle, pas);
-                case 1:
-                    sizex += Generator.Next(3, 10);
-                    copyx = sizex;
-                    intensity = (float)(Generator.Next(20, 35)) / 10;
-                    frequency = (float)(Generator.Next(5, 9));
-                    return (pas, z) => Mathf.Lerp(0, intensity * Mathf.Sin(pas * frequency), pas);
-                case 2:
-                    angle = (float)(Generator.Next(3, 8)) / 10;
-                    sizex += Generator.Next(3, 8);
-                    copyx = sizex;
-                    if (negative > 0) copyx = -sizex;
-                    intensity = (float)(Generator.Next(20, 35)) / 10;
-                    frequency = (float)(Generator.Next(5, 9));
-                    return (pas, z) => Mathf.Lerp(0, copyx * angle + (intensity * Mathf.Sin(pas * frequency)), pas);
-            }
-        }
-        return (pas, z) => Mathf.Lerp(0, 0, pas);
-    }
-
-    MeshAndPos GenerateMutatedPlaneXZ(Vector2 size, int nSegmentsX, int nSegmentsZ, TwoVariablesFunction heightFunction, TwoVariablesFunction widthFunction, TwoVariablesFunction zFunction)
+    MeshAndPos GenerateMutatedPlaneXZ(Vector2 size, int nSegmentsX, int nSegmentsZ, Mutations.TwoVariablesFunction heightFunction, Mutations.TwoVariablesFunction widthFunction, Mutations.TwoVariablesFunction zFunction, Vector2 absolutepos)
 
     {
         Mesh mesh = new Mesh();
         mesh.name = "plane";
 
-        Vector3[] vertices = new Vector3[(nSegmentsX + 1) * (nSegmentsZ + 1)];
-        int[] triangles = new int[nSegmentsX * nSegmentsZ * 2 * 3]; //  
+        Vector3[] vertices = new Vector3[(nSegmentsX + 1) * (nSegmentsZ + 1) * 2];
+        int[] triangles = new int[(4*3) + ((nSegmentsX * nSegmentsZ) + nSegmentsX) * 2 * 3 * 2];
         Vector3[] normals = new Vector3[vertices.Length];
         Vector2[] uvs = new Vector2[vertices.Length];
 
-        Vector2 halfSize = size * .5f;
+        int vDownOff = (nSegmentsX + 1) * (nSegmentsZ + 1);
+        int tDownOff = nSegmentsX * nSegmentsZ * 2 * 3 - 1;
 
-        float x=0;
-        float y=0;
-        float z=0;
+        float x = 0;
+        float y = 0;
+        float z = 0;
         float width = 0;
 
         // remplissage des vertices, normals, uv
@@ -225,28 +133,37 @@ public class meshProc : MonoBehaviour
         {
             int offset = (nSegmentsX + 1) * i;
             float kZ = (float)i / nSegmentsZ;
+
             for (int j = 0; j < nSegmentsX + 1; j++)
             {
                 float kX = (float)j / nSegmentsX;
-                width = size.y+widthFunction(kX*2, kZ);
+                width = size.y + widthFunction(kX * 2, kZ);
 
                 x = Mathf.Lerp(0, size.x, kX);
-                z = Mathf.Lerp(-width/2, width / 2, kZ)+ zFunction(kX,kZ);
+                z = Mathf.Lerp(-width / 2, width / 2, kZ) + zFunction(kX, kZ);
                 y = heightFunction(kX, kZ);
-
+                
+                //UP
                 vertices[offset + j] = new Vector3(x, y, z);
                 normals[offset + j] = Vector3.up;
-                uvs[offset + j] = new Vector2(kX, kZ);
+                uvs[offset + j] = new Vector2(absolutepos.x + x, absolutepos.y + z);
+
+                //DOWN
+                vertices[(offset + j) + vDownOff] = new Vector3(x, y - 1, z);
+                normals[(offset + j) + vDownOff] = Vector3.down;
+                //uvs[(offset + j) + vDownOff] = new Vector2(kX, kZ);
             }
         }
 
         // remplissage de triangles
         int index = 0;
+        int index2 = tDownOff + 1;
         for (int i = 0; i < nSegmentsZ; i++)
         {
             int offset = (nSegmentsX + 1) * i;
             for (int j = 0; j < nSegmentsX; j++)
             {
+                //UP
                 triangles[index++] = offset + j;
                 triangles[index++] = offset + j + nSegmentsX + 1;
                 triangles[index++] = offset + j + nSegmentsX + 1 + 1;
@@ -254,16 +171,65 @@ public class meshProc : MonoBehaviour
                 triangles[index++] = offset + j;
                 triangles[index++] = offset + j + nSegmentsX + 1 + 1;
                 triangles[index++] = offset + j + 1;
+
+                //DOWN
+                triangles[index2++] = offset + j + vDownOff;
+                triangles[index2++] = offset + j + nSegmentsX + 1 + 1 + vDownOff;
+                triangles[index2++] = offset + j + nSegmentsX + 1 + vDownOff;
+
+                triangles[index2++] = offset + j + vDownOff;
+                triangles[index2++] = offset + j + 1 + vDownOff;
+                triangles[index2++] = offset + j + nSegmentsX + 1 + 1 + vDownOff; 
             }
         }
+
+        int index3 = index2;
+        int index4 = index3 + (nSegmentsX * 2 * 3);
+        // remplissage des triangles sur les cotes
+        for (int i = 0; i < nSegmentsX; i++) {
+            //RIGHT
+            triangles[index3++] = i;
+            triangles[index3++] = i + 1;
+            triangles[index3++] = i + vDownOff +1;
+
+            triangles[index3++] = i;
+            triangles[index3++] = i + vDownOff + 1;
+            triangles[index3++] = i + vDownOff;
+
+            //LEFT
+            triangles[index4++] = i + nSegmentsZ + (nSegmentsZ*nSegmentsX);
+            triangles[index4++] = i + vDownOff + nSegmentsZ + (nSegmentsZ * nSegmentsX);
+            triangles[index4++] = i + nSegmentsZ + 1 + (nSegmentsZ * nSegmentsX);
+
+            triangles[index4++] = i + nSegmentsZ + 1 + (nSegmentsZ * nSegmentsX);
+            triangles[index4++] = i + vDownOff + nSegmentsZ + (nSegmentsZ * nSegmentsX);
+            triangles[index4++] = i + vDownOff + nSegmentsZ + 1 + (nSegmentsZ * nSegmentsX);
+        }
+
+        //remplissage triangles Back
+        triangles[index4++] = 0;
+        triangles[index4++] = vDownOff;
+        triangles[index4++] = nSegmentsZ + (nSegmentsZ*nSegmentsX) + vDownOff;
+        triangles[index4++] = 0;
+        triangles[index4++] = nSegmentsZ + (nSegmentsZ * nSegmentsX) + vDownOff;
+        triangles[index4++] = nSegmentsZ + (nSegmentsZ * nSegmentsX);
+
+        //remplissage triangles Front
+        triangles[index4++] = 0 + nSegmentsX;
+        triangles[index4++] = nSegmentsZ + (nSegmentsZ * nSegmentsX) + vDownOff + nSegmentsX;
+        triangles[index4++] = vDownOff + nSegmentsX;
+        triangles[index4++] = 0 + nSegmentsX;
+        triangles[index4++] = nSegmentsZ + (nSegmentsZ * nSegmentsX) + nSegmentsX;
+        triangles[index4++] = nSegmentsZ + (nSegmentsZ * nSegmentsX) + vDownOff + nSegmentsX;
 
         mesh.vertices = vertices;
         mesh.triangles = triangles;
         mesh.normals = normals;
         mesh.uv = uvs;
 
-        mesh.RecalculateNormals();
         mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
 
         MeshAndPos result = new MeshAndPos(mesh, new Vector3(x,y,z),width);
 
